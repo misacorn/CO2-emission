ec45fefc2c4f88dc5d36c43764076ed60c5128bd
Misa <dieuhuongngo@gmail.com>
2019-11-25 22:38:25 +0200

    fetching data from API with authorization

--- components/mainPage/Categories.js
+++ components/mainPage/Categories.js
@@ -1,10 +1,41 @@
-import React from "react";
-import { View, Text, StyleSheet } from "react-native";
+import React, { useState, useEffect } from "react";
+import { FlatList, View, Text, StyleSheet } from "react-native";
+
+import config from "../../config";
 
 const Categories = () => {
+  const [domains, setDomains] = useState([]);
+
+  useEffect(() => {
+    const url = `${config.BASE_URL}/v1/byDomain/${config.CUSTOMER_ID}?auditUser=${config.AUDIT_USER}&timePeriod=Monthly`;
+    fetch(url, {
+      method: "GET",
+      headers: {
+        Authorization: `${config.AUTHORIZATION}`
+      }
+    })
+      .then(response => response.json())
+      .then(responseJson => setDomains(responseJson.listbyDomain))
+      .catch(error => {
+        console.error(error);
+      });
+  }, []);
+
   return (
     <View style={styles.container}>
-      <Text>Categories here</Text>
+      {domains.length > 0 && (
+        <FlatList
+          data={domains}
+          renderItem={({ item }) => {
+            return (
+              <View>
+                <Text>{item.co2Amount}</Text>
+              </View>
+            );
+          }}
+          keyExtractor={item => item.domain}
+        />
+      )}
     </View>
   );
 };
---
+++ config.js
@@ -0,0 +1,14 @@
+import {
+  BASE_URL,
+  AUDIT_USER,
+  CUSTOMER_ID,
+  AUTHORIZATION
+} from "react-native-dotenv";
+
+
+export default {
+  BASE_URL,
+  AUDIT_USER,
+  CUSTOMER_ID,
+  AUTHORIZATION
+};
