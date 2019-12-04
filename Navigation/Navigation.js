8cf5d81e7abd6bdc5416784551dbd4316ae6ad1b
Misa <dieuhuongngo@gmail.com>
2019-12-04 14:43:47 +0200

    changed folder name

--- Navigation/Navigation.js
+++ Navigation/Navigation.js
@@ -1,9 +1,9 @@
 import { createAppContainer } from "react-navigation";
 import { createStackNavigator } from "react-navigation-stack";
 
-import Main from "../Screens/Main";
-import Profile from "../Screens/Profile";
-import Tips from "../Screens/Tips";
+import Main from "../screens/Main";
+import Profile from "../screens/Profile";
+import Tips from "../screens/Tips";
 
 const Navigation = createStackNavigator(
   {
